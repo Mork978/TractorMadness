f8bdf52d226a678781924225288e1ed46ce8640b
teleportex <guillem.ramirez.miranda@est.fib.upc.edu>
2017-11-18 13:21:39 +0100

    moving tractor

--- Scripts/tractor.lua
+++ Scripts/tractor.lua
@@ -1,4 +1,4 @@
-tractor = {
+tractorClass = {
 
   new = function(self, x, y) -- This function creates and returns a new tractor object, as an instance of tractor
 
@@ -7,16 +7,38 @@ tractor = {
     -- Properties
     tractor.imageFilename = "Textures/placeholder.jpg"
     tractor.imageFile = love.graphics.newImage(tractor.imageFilename)
-    tractor.x = x
-    tractor.y = y
+    tractor.pos = {x = x, y = y}
     tractor.offset = {x = tractor.imageFile:getWidth() / 2, y = tractor.imageFile:getHeight() / 2}
-
+    tractor.forwardVec = {0, -1}
     -- Methods
     tractor.draw = self.draw
+    tractor.move = self.move
     return tractor
   end,
-
+  
   draw = function(self)
     love.graphics.draw(self.imageFile, self.pos.x - self.offset.x, self.pos.y - self.offset.y)
+  end,
+  
+  move = function(self, w, h, dt) -- This function moves tractor object in window, by its position (x, y)
+
+    local moveDirections = {
+      right = {x = 1, y = 0},
+      left = {x = -1, y = 0},
+      up = {x = 0, y = -1},
+      down = {x = 0, y = 1}
+    }
+    for direction, forwardVec in pairs (moveDirections) do
+      if love.keyboard.isDown(direction) then
+        self.forwardVec = forwardVec
+        self.pos.x = self.pos.x + self.forwardVec.x *300* dt
+        self.pos.y = self.pos.y + self.forwardVec.y *300* dt
+
+        self.pos.x = clamp(self.pos.x, self.offset.x, w - self.offset.x) -- We clamp x values according to window dimensions
+        self.pos.y = clamp(self.pos.y, self.offset.y, h - self.offset.y) -- We clamp y values according to window dimensions
+      end
+    end
   end
+    
+
 }
--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
 
-
+dofile('Scripts/tractor.lua')
 dofile('Scripts/auxFunctions.lua')
 dofile('Scripts/auxClasses.lua')
 dofile('Scripts/updateFunctions.lua')
 dofile('Scripts/drawFunctions.lua')
 
 local gameIniParameters = dofile('tractor.ini') -- Load game ini parameters from ini file
-
+local tractor
 local w, h -- Global (local chunk) variables for window dimensions
 
 
@@ -23,8 +23,8 @@ function updateGameStates(gameEvent)
 end
 
 function love.load()
-
   w, h = love.graphics.getDimensions() -- Sets w,h to window dimensions
+  tractor = tractorClass:new(w/2, h/2)
 
 
 end
@@ -32,11 +32,11 @@ end
 
 
 function love.update(dt)
-
+  tractor:move(w, h,dt)
 end
 
 
 function love.draw()
-
+  tractor:draw()
 
 end
